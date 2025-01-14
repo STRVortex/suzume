dd922a0fcc02fd69f37def6ac87cc7be61aa5923
koreansmu <shuklakusum4q@gmail.com>
2025-01-15 01:42:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/koreansmu/Suzume-Import",
+    "https://github.com/koreansmu/SuzumeeLV",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
